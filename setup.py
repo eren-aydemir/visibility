0b9e5d66fa3055bfaa2261e77bdd4386ee97d17a
Eren Aydemir <eren.aydemir@gmail.com>
2023-07-23 15:53:07 +0300

    Update setup.py nvcc_args

--- setup.py
+++ setup.py
@@ -8,6 +8,19 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+cxx_args = ['-std=c++14']
+
+nvcc_args = [
+    # '-gencode', 'arch=compute_50,code=sm_50',
+    # '-gencode', 'arch=compute_52,code=sm_52',
+    # '-gencode', 'arch=compute_60,code=sm_60',
+    # '-gencode', 'arch=compute_61,code=sm_61',
+    # '-gencode', 'arch=compute_70,code=sm_70',
+    '-gencode', 'arch=compute_70,code=compute_70',
+    '-gencode', 'arch=compute_75,code=compute_75',
+    '-gencode', 'arch=compute_86,code=compute_86'
+]
+
 setup(
     name='visibility',
     version='0.1',
@@ -18,7 +31,7 @@ setup(
         CUDAExtension('visibility', [
             'src/visibility.cpp',
             'src/visibility_kernel.cu',
-        ])
+        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
     ],
     cmdclass={
         'build_ext': BuildExtension
